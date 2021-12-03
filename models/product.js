e23d38b761bfb91b58fe71d8f2d89a2b3a34dbad
Jeremy <leezonghan1997@gmail.com>
2021-12-03 21:15:25 +0800

    first commit

---
+++ models/product.js
@@ -0,0 +1,77 @@
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
+
+class Product {
+  constructor(title, price, description, imageUrl, _id) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this._id = _id ? new mongodb.ObjectId(_id) : null;
+  }
+
+  save() {
+    const db = getDb();
+    let dbOp;
+
+    if (this._id) {
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbOp = db.collection("products").insertOne(this);
+    }
+    return dbOp
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find() // return Cursor Object
+      .toArray() // to get all documents and turn it into JS Array
+      .then((products) => {
+        console.log(products);
+        return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static fetchById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        console.log(product);
+        return product;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => {
+        console.log("DELETED!");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+
+module.exports = Product;
---
+++ util/database.js
@@ -0,0 +1,29 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://zh-lee_1997:WN28dzlvO3nbBRkS@cluster0.5jbu8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
